d0c5b7b86c93ef071b246d1266d8599116f71a99
Bato Unen <batounen@gmail.com>
2022-08-26 10:39:44 -0600

    Return Fibonacci number at given index

---
+++ src/FibonacciNumbers.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class FibonacciNumbers {
+
+    /*
+    The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+    F(0) = 0, F(1) = 1
+    F(n) = F(n - 1) + F(n - 2), for n > 1.
+    Given n, calculate F(n).
+    Example 1:
+        Input: n = 2
+        Output: 1
+        Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+    Example 2:
+        Input: n = 3
+        Output: 2
+        Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+    Example 3:
+        Input: n = 4
+        Output: 3
+        Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+     */
+
+
+    // 0 ms, faster than 100.00% & 40.9 MB, less than 40.48%
+    public int fib(int n) {
+
+        ArrayList<Integer> fiboNumbers = new ArrayList<>();
+        fiboNumbers.add(0);
+        fiboNumbers.add(1);
+
+        for (int i = 2; i <= 30; i++) {
+            fiboNumbers.add(fiboNumbers.get(i - 2) + fiboNumbers.get(i - 1));
+        }
+        return fiboNumbers.get(n);
+    }
+}
