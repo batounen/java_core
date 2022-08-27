b3b99ea5c9e13b430cae0347be7c6af64f781ae5
Bato Unen <batounen@gmail.com>
2022-08-27 10:42:37 -0600

    Find frequency of characters without duplication

---
+++ src/FrequencyOfCharacters.java
@@ -0,0 +1,29 @@
+public class FrequencyOfCharacters {
+
+    /*
+        given a string determine how many times a char is in the string
+          java:
+            j - 1
+            a - 2
+            v - 1
+     */
+
+    public static void main(String[] args) {
+
+        String str = "aaabbccccdddee";
+        String checked = "";
+
+        for (int i = 0; i < str.length(); i++) {            // outer: iterating one character at a time --> charAt(i)
+            int counter = 0;                                // the counter should reset back to 0 for every character
+            if (!checked.contains("" + str.charAt(i))) {
+                for (int j = 0; j < str.length(); j++) {    // inner: compare the character we are looking at( i ) with each other character in the String --> str.charAt(j)
+                    if (str.charAt(i) == str.charAt(j)) {
+                        counter++;
+                    }
+                }
+                System.out.println(str.charAt(i) + " - " + counter);
+                checked += str.charAt(i);                               // a won't be checked and run twice
+            }
+        }
+    }
+}
