4d316d7cd12c69bf3ee90dcffc1741bc0082e6d3
Bato Unen <batounen@gmail.com>
2022-09-04 10:15:24 -0600

    return sum of digits from the given int number

---
+++ src/SumOfDigits.java
@@ -0,0 +1,16 @@
+public class SumOfDigits {
+
+    public static void main(String[] args) {
+
+        // Example: 2561 -> 2+5+6+1=14
+
+        int n = 2561;
+        int sum = 0;
+
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        System.out.println(sum);
+    }
+}
