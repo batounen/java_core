d18891c8db0c950a221dd030c56daa35f02ff1d1
Bato Unen <batounen@gmail.com>
2022-09-04 10:12:13 -0600

    primer number checker

---
+++ src/PrimeNumber.java
@@ -0,0 +1,50 @@
+public class PrimeNumber {
+
+    /*
+    Prime number
+        Give a number determine if it is a prime number. A prime number is a number that is only divisible by 1 and itself.
+        Ex:
+            Input:
+                11
+            Output:
+                prime
+        Ex:
+            Input:
+                10
+            Output:
+                not prime
+     */
+
+    public static void main(String[] args) {
+
+        // OPTION 1
+        int num = 8, count = 0;
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                count++;
+                break;
+            }
+        }
+        if (count == 0 && num != 1) {
+            System.out.println("Number is Prime: " + num);
+        } else {
+            System.out.println("Number is Not Prime: " + num);
+        }
+
+
+        // OPTION 2
+        int n = 12;
+        boolean isPrime = true;
+        if (n < 2) {
+            System.out.println("Not prime");
+            System.exit(0);
+        }
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        System.out.println(isPrime ? "Prime" : "Not Prime");
+    }
+}
