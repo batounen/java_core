45434b5af66bccb2927bf3a14bd67598e3545248
Bato Unen <batounen@gmail.com>
2022-09-04 10:13:07 -0600

    remove any extra spaces from the given string

---
+++ src/RemoveExtraSpaces.java
@@ -0,0 +1,12 @@
+public class RemoveExtraSpaces {
+
+    public static void main(String[] args) {
+
+        String str = "  Too       many    spaces    to    remove   from this     string.   ";
+
+        while(str.contains("  ")) {
+            str = str.replace("  ", " ");
+        }
+        System.out.println(str.strip());
+    }
+}
