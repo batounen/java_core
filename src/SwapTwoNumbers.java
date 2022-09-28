62fae0d496fb28c60ef71da50c6fcf0d96c4bdb8
Bato Unen <batounen@gmail.com>
2022-09-28 11:08:59 -0600

    swap 2 numbers without third variable

---
+++ src/SwapTwoNumbers.java
@@ -0,0 +1,16 @@
+public class SwapTwoNumbers {
+    public static void main(String[] args) {
+
+        // Swap two number variable values without using a third variable
+
+        int a = 5;
+        int b = 11;
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+    }
+}
