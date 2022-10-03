f80b68cef8489c2f032e03a3576fa104f857cf8e
Bato Unen <batounen@gmail.com>
2022-10-03 11:42:09 -0600

    RemoveDupesFromSortedArray updated

--- src/RemoveDupesFromSortedArray.java
+++ src/RemoveDupesFromSortedArray.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class RemoveDupesFromSortedArray {
 
     /*
@@ -30,8 +32,9 @@ public class RemoveDupesFromSortedArray {
 
     public static void main(String[] args) {
 
-        int[] nums = new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
         System.out.println(removeDuplicates(nums));                 // 5
+        System.out.println(Arrays.toString(nums));               // [0, 1, 2, 3, 4, 2, 2, 3, 3, 4]
 
     }
 }
