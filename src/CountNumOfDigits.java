329b21fdbf25ba9f1b7f51f2d483430fc0cb3933
Bato Unen <batounen@gmail.com>
2022-10-17 17:27:02 -0600

    count number of digits in a number

---
+++ src/CountNumOfDigits.java
@@ -0,0 +1,28 @@
+public class CountNumOfDigits {
+
+    /*
+    Write a java program to count number of digits in a number
+     */
+
+    // String Way
+    public static int numCounter1(int a) {
+        String number = String.valueOf(a);
+        return number.length();
+    }
+
+    // Number way
+    public static int numCounter2(int a) {
+        int digit = 0;
+        while (a > 0) {
+            a /= 10;
+            digit++;
+        }
+        return digit;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(numCounter1(123098));
+        System.out.println(numCounter2(12309));
+    }
+
+}
