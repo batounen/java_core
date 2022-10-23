0afd374187735a0afedf12c65188f7e301eef315
Bato Unen <batounen@gmail.com>
2022-10-23 11:47:11 -0600

    FrequencyOfCharacters updated

--- src/FrequencyOfCharacters.java
+++ src/FrequencyOfCharacters.java
@@ -11,18 +11,18 @@ public class FrequencyOfCharacters {
     public static void main(String[] args) {
 
         String str = "aaabbccccdddee";
-        String checked = "";
+        StringBuilder checked = new StringBuilder();
 
         for (int i = 0; i < str.length(); i++) {            // outer: iterating one character at a time --> charAt(i)
             int counter = 0;                                // the counter should reset back to 0 for every character
-            if (!checked.contains("" + str.charAt(i))) {
+            if (!checked.toString().contains("" + str.charAt(i))) {
                 for (int j = 0; j < str.length(); j++) {    // inner: compare the character we are looking at( i ) with each other character in the String --> str.charAt(j)
                     if (str.charAt(i) == str.charAt(j)) {
                         counter++;
                     }
                 }
                 System.out.println(str.charAt(i) + " - " + counter);
-                checked += str.charAt(i);                               // a won't be checked and run twice
+                checked.append(str.charAt(i));                               // a won't be checked and run twice
             }
         }
     }
