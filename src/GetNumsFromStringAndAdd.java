1a80809525ab1239d9a41d68b3394156f09434e5
Bato Unen <batounen@gmail.com>
2022-10-23 12:00:45 -0600

    GetNumsFromStringAndAdd updated

--- src/GetNumsFromStringAndAdd.java
+++ src/GetNumsFromStringAndAdd.java
@@ -2,44 +2,34 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class GetNumsFromStringAndAdd {
-
     public static void main(String[] args) {
-
         String str = "Thereare500mangoes on30 trees in 5 gardens";       // need output = 535 (500+30+5)
 
         // OPTION 1
-        String output = "";
+        StringBuilder output = new StringBuilder();
         int total = 0;
-
         for (int i = 0; i < str.length(); i++) {
             if ((str.charAt(i) >= '0' && str.charAt(i) <= '9')) {
-                output += str.charAt(i);
-            } else if (!output.isEmpty() && str.charAt(i) == ' ') {
-                total += Integer.parseInt(output);
-                output = "";
+                output.append(str.charAt(i));
+            } else if (output.length() != 0 && str.charAt(i) == ' ') {
+                total += Integer.parseInt(output.toString());
+                output.setLength(0);
             }
         }
-        System.out.println(total);
+        System.out.println("total = " + total);
 
 
         // OPTION 2 - Wrapper class
-        String result = "";
+        StringBuilder result = new StringBuilder();
         int sum = 0;
-
         for (int i = 0; i < str.length(); i++) {
-
-            char current = str.charAt(i);
-
-            if ((i == str.length() - 1) && Character.isDigit(current)) {
-                result += "" + current;
-                sum += Integer.parseInt(result);
-            } else if (Character.isDigit(current)) {
-                result += "" + current;
-            } else if (!result.isEmpty() && !Character.isDigit(current)) {
-                sum += Integer.parseInt(result);
-                result = "";
+            char currentChar = str.charAt(i);
+            if ((Character.isDigit(currentChar))) {
+                result.append(currentChar);
+            } else if (result.length() != 0 && !Character.isDigit(currentChar)) {
+                sum += Integer.parseInt(result.toString());
+                result.setLength(0);
             }
-
         }
         System.out.println("sum = " + sum);         // sum = 535
 
@@ -47,16 +37,15 @@ public class GetNumsFromStringAndAdd {
         // OPTION 3 - ArrayList
         ArrayList<String> s = new ArrayList<>(Arrays.asList(str.split(" ")));
         int sumOfAll = 0;
-
         for (String each : s) {
-            String res = "";
+            StringBuilder res = new StringBuilder();
             for (int i = 0; i < each.length(); i++) {
                 if (Character.isDigit(each.charAt(i))) {
-                    res += "" + each.charAt(i);
+                    res.append(each.charAt(i));
                 }
             }
-            if (!res.isEmpty()) {
-                sumOfAll += Integer.parseInt(res);
+            if (res.length() != 0) {
+                sumOfAll += Integer.parseInt(res.toString());
             }
         }
         System.out.println("sumOfAll = " + sumOfAll);
