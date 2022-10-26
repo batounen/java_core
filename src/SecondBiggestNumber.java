9c085a90a33ea099168fb12942f5ce31f675be87
Bato Unen <batounen@gmail.com>
2022-10-26 14:20:53 -0600

    SecondBiggestNumber updated

--- src/SecondBiggestNumber.java
+++ src/SecondBiggestNumber.java
@@ -16,13 +16,11 @@ public class SecondBiggestNumber {
      */
 
     public static void main(String[] args) {
-
         int[] nums = {4, 3, 1, 4, 5, 2, 4, 8, 4, 8};
 
         // OPTION 1
         Arrays.sort(nums);
         System.out.println(Arrays.toString(nums));
-
         for (int i = nums.length - 1; i > 0; i--) {
             if (nums[i] != nums[nums.length - 1]) {
                 System.out.println("The second biggest number in the given array is " + nums[i]);
@@ -33,7 +31,6 @@ public class SecondBiggestNumber {
 
         // OPTION 2
         ArrayList<Integer> newNums = new ArrayList<>();
-
         for (int i = nums.length - 1; i >= 0; i--) {
             int numCount = 0;
             for (int j = nums.length - 1; j >= 0; j--) {
