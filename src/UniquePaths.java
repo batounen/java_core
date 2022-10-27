ac65acd027d7a41073bfd6393f47d4937a5239f2
Bato Unen <batounen@gmail.com>
2022-10-27 08:08:27 -0600

    UniquePaths updated

--- src/UniquePaths.java
+++ src/UniquePaths.java
@@ -18,17 +18,13 @@ public class UniquePaths {
 
     // 0 ms, faster than 100.00% & 39.2 MB, less than 90.82%
     public int uniquePaths(int m, int n) {
-
         int[][] paths = new int[m][n];
-
         for (int i = 0; i < m; i++) {
             paths[i][0] = 1;
         }
-
         for (int j = 0; j < n; j++) {
             paths[0][j] = 1;
         }
-
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 paths[i][j] = paths[i - 1][j] + paths[i][j - 1];
